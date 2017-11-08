805ea9d88da8ca386fa6b9a14ae2866aa0841a0d
Henry Yang <henryyang0217@gmail.com>
2017-11-07 23:26:49 -0800

    Create Session

--- src/main/java/com/hanyang/contactmgr/Application.java
+++ src/main/java/com/hanyang/contactmgr/Application.java
@@ -1,7 +1,42 @@
 package com.hanyang.contactmgr;
 
-public class Application {
-    public static void main(String[] args) {
+import com.hanyang.contactmgr.model.Contact;
+import com.hanyang.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import org.hibernate.service.ServiceRegistry;
+
+public class Application {
+    // Hold a reusable reference to a SessionFactory (since we need only one)
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        // Create a StandardServiceRegistry
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    public static void main(String[] args) {
+        Contact contact = new ContactBuilder("Chris", "Ramacciotti")
+                .withEmail("rama@treehouse.com")
+                .withPhone(7766551234L)
+                .build();
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Begin a transaction
+        session.beginTransaction();
+
+        // Use the session to save the contact
+        session.save(contact);
+
+        // Commit the transaction
+        session.getTransaction().commit();
+
+        // Close the session
+        session.close();
     }
 }
